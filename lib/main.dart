f54593b2622969884af75030d2803a9069ff29b5
Maxim <incentivedie@gmail.com>
2024-10-22 14:33:32 +0400

    Completed app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'network_service.dart';
+import 'post.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,17 +25,44 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final NetworkService _networkService = NetworkService();
+  late Future<List<Post>> _futurePosts;
 
   @override
   void initState() {
     super.initState();
-    print("initState");
-    _networkService.fetchData();
+    _futurePosts = _networkService.fetchData();
   }
 
   @override
   Widget build(BuildContext context) {
-    print("build");
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text("Posts")),
+      body: FutureBuilder(
+        future: _futurePosts,
+        builder: ((context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text("Error!"));
+          } else if (!snapshot.hasData) {
+            return Center(child: Text("No data!"));
+          } else {
+            final List<Post> posts = snapshot.data!;
+            return ListView.separated(
+              itemCount: posts.length,
+              separatorBuilder: (context, index) => Divider(),
+              itemBuilder: (context, index) {
+                final Post post = posts[index];
+                return ListTile(
+                  title: Text(post.title),
+                  subtitle: Text(post.body),
+                  leading: Text("${post.id}"),
+                );
+              },
+            );
+          }
+        }),
+      ),
+    );
   }
 }
--- lib/network_service.dart
+++ lib/network_service.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
 
+import 'post.dart';
+
 final class NetworkService {
   // Экземпляр класса Dio для взаимодействие с сетью
   final Dio _dio = Dio();
@@ -8,8 +10,20 @@ final class NetworkService {
   final String url = "https://jsonplaceholder.typicode.com/posts";
 
   // Метод получения данных по url
-  void fetchData() async {
-    final responce = await _dio.get(url);
-    print(responce.data);
+  Future<List<Post>> fetchData() async {
+    try {
+      final responce = await _dio.get(url);
+      return (responce.data as List)
+          .map((json) => Post.fromJson(json))
+          .toList();
+    } catch (error) {
+      print("Error: $error"); // в случае ошибки выводим ее в консоль
+      throw Exception("Failed to load data"); // выбрасываем исключение на экран
+    }
   }
 }
+
+
+// final rawData = responce.data as List; // сохраняю в новую переменную, пытаясь привести к типу List
+// var postList = rawData.map((json) => Post.fromJson(json)); // Проходимся по rawData через map и создаем объекты класса Post
+// var posts = postList.toList(); // Получившийся результат приводим к List
